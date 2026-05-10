6229cfb032c5504d126b694685b882e2b0506e48
solomon <Kmantalgosolo@gmail.com>
2026-05-10 22:43:39 +0300

    Changes

--- server/src/server.js
+++ server/src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const http = require('http');
 const helmet = require('helmet');
 const compression = require('compression');
+const cors = require('cors');
 const db = require('./config/db');
 
 const app = express();
