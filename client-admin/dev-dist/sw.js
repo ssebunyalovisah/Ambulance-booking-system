f3d1368470f3b99c8d8344fdeb15d96756bddf84
ssebunyalovisah <ssebunyalovisah@gmail.com>
2026-05-10 22:22:18 +0300

    Save local changes

--- client-admin/dev-dist/sw.js
+++ client-admin/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q955l1gosd"
+    "revision": "0.joh1jq19714"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
